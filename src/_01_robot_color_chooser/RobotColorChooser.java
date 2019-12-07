4ee084f85d3d37f8b35e5bea3e4e1750d6be7fa9
League Student <league@iMac-14.attlocal.net>
2019-12-06 16:59:51 -0800

    finished mod 2
    on crazy cat lady. finished robot color chooser

--- src/_01_robot_color_chooser/RobotColorChooser.java
+++ src/_01_robot_color_chooser/RobotColorChooser.java
@@ -4,24 +4,45 @@
 package _01_robot_color_chooser;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+import java.awt.AWTException;
 import java.awt.Color;
 
+
 public class RobotColorChooser {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws AWTException {
 		//1. Create a new Robot
-		
+		Robot r =new Robot();
 		//3. Ask the user what color they would like the robot to draw
+
 		
+		String hello = JOptionPane.showInputDialog(null,"choose a color: red, orange, yellow..");
 		//5. Use an if/else statement to set the pen color that the user requested
-
-        //6. If the user doesn’t enter anything, choose a random color
-
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//4. Set the pen width to 10
-		
-	    //2. Make the robot draw a shape (this will take more than one line of code)
-
-
-	}
+if(hello.equalsIgnoreCase("red") ) {
+	r.setPenColor(255,45,0);
+}else if(hello.equalsIgnoreCase("orange")) {
+	r.setPenColor(255,151,0);
+}else if(hello.equalsIgnoreCase("yellow")) {
+	r.setPenColor(255,246,0);
+}else if(hello.equalsIgnoreCase("green")) {
+	r.setPenColor(77,255,0);
+}else if(hello.equalsIgnoreCase("blue")) {
+	r.setPenColor(139,255,0);
+}else if(hello.equalsIgnoreCase("purple")) {
+	r.setPenColor(108,0,255);
 }
+	// 6. If the user doesn’t enter anything, choose a random color
+
+	// 7. Put a loop around your code so that you keep asking the user for more
+	// colors & drawing them
+
+	// 4. Set the pen width to 10
+r.setPenWidth(10);
+	// 2. Make the robot draw a shape (this will take more than one line of code)
+r.setSpeed(60);
+r.penDown();
+r.turn(90);
+r.move(200);
+	}}
